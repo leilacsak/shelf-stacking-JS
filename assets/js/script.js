ae2d35137fb2951aca445372c4e8add2aef237bc
leilacsak <leilacsak@gmail.com>
2025-12-17 17:27:21 +0000

    Grab the shelf divs by their Id

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,9 @@
 fruits = ["🍌", "🍏", "🍊", "🍌", "🍌", "🍏", "🍊", "🍏", "🍊", "🍊", "🍏", "🍌"];
 
+const bananaShelf = document.getElementById("banana-shelf");
+const orangeShelf = document.getElementById("orange-shelf"); 
+const appleShelf = document.getElementById("apple-shelf");
 
+
+ 
 
